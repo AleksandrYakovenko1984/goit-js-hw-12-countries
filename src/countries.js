0eb2c71af718a117457d7d88700e94657b8765bd
AleksandrYakovenko1984 <jakovenko.gav@gmail.com>
2021-11-24 17:12:08 +0200

    clearValues

--- src/countries.js
+++ src/countries.js
@@ -16,10 +16,12 @@ searchBox.addEventListener('input', debounce(() => {
 
 function showCountry(listCountries) {
     let template = ""
-    if (listCountries.length > 10) {
+  if (listCountries.length > 10) {
+      clearValues()
        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
     // countryList.innerHTML = template;
     } else if (listCountries.length < 10 && listCountries.length > 1) {
+      clearValues()
          listCountries.forEach((e) => {
          template = template + listEveryCountries(e)
         }) 
@@ -28,12 +30,17 @@ function showCountry(listCountries) {
          countryList.innerHTML = template;
        
     } else if (listCountries.length === 1) {
+      clearValues()
          listCountries.forEach((e) => {
          template = template + listOneCountries(e);
     
        })
          countryList.innerHTML = template;
     } 
+}
+
+function clearValues(){
+countryList.innerHTML = ""
 }
 function showError() {
  
